eafefa7a346f43e7fbb41d690aff3beb5aa1674c
Kirill <kill4ik2002@gmail.com>
2024-11-30 14:29:46 +0300

    update crud

--- src/database/database.py
+++ src/database/database.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy import Column, Integer, BigInteger, String, Numeric, TIMESTAMP
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 load_dotenv()
@@ -9,10 +8,4 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(
-    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine)
-Base = declarative_base()
-
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine, class_=AsyncSession)
--- src/database/postgres_service.py
+++ src/database/postgres_service.py
@@ -1,15 +1,85 @@
-from sqlalchemy import insert, delete
-from src.database.database import async_session_maker
-from src.database.models import User, Transaction
+from sqlalchemy import insert, select
+from src.database.database import async_session_maker, engine
+from src.models import User, Base, Transaction, Portfolio
+
+
+async def init_db():
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        print("Database initialized successully")
+    except Exception as e:
+        print(f"Exception: {e}")
 
 
 class PostgresManager:
-    async def add_user(self, telegram_id: int, username: str, bio: str) -> None:
+    async def add_user(self, telegram_id: int, username: str) -> None:
         async with async_session_maker() as session:
             stm = insert(User).values(
                 telegram_id=telegram_id,
-                username=username,
-                bio=bio
+                username=username
             )
             await session.execute(stm)
             await session.commit()
+
+    async def user_exists(self, telegram_id: int) -> bool:
+        async with async_session_maker() as session:
+            result = await session.execute(
+                select(User).filter_by(telegram_id=telegram_id)
+            )
+            user = result.scalars().first()
+            return user is not None
+
+    async def get_user_by_telegram_id(self, telegram_id: int) -> User:
+        async with async_session_maker() as session:
+            result = session.query(User).filter(
+                User.telegram_id == telegram_id).first()
+            return result
+
+    async def update_user_balance(self, telegram_id: int, new_balance: float):
+        async with async_session_maker() as session:
+            user = session.query(User).filter(telegram_id=telegram_id)
+            if user:
+                user.balance = new_balance
+                session.commit()
+
+    async def create_transaction(self, user_id: int, type: str, asset: str, amount: int, price: float, total: float):
+        async with async_session_maker() as session:
+            transaction = Transaction(
+                user_id=user_id,
+                type=type,
+                asset=asset,
+                amount=amount,
+                price=price,
+                total=total
+            )
+            session.add(transaction)
+            session.commit()
+
+    async def get_transactions_by_user_id(self, user_id: int):
+        async with async_session_maker() as session:
+            result = session.query(Transaction).filter(
+                Transaction.user_id == user_id).all()
+
+    async def upsert_portfolio(self, user_id: int, asset: str, quantity: int, value: float):
+        async with async_session_maker() as session:
+            portfolio = session.query(Portfolio).filter(
+                Portfolio.user_id == user_id,
+                Portfolio.asset == asset
+            ).first()
+
+            if portfolio:
+                old_quantity = portfolio.quantity
+                old_value = portfolio.value
+                portfolio.quantity += + quantity
+
+                if value != old_value:
+                    new_value = ((old_value * old_quantity) +
+                                 (value * quantity)) / (old_quantity + quantity)
+                    portfolio.value = new_value
+            else:
+                portfolio = Portfolio(
+                    user_id=user_id, asset=asset, quantity=quantity, value=value)
+                session.add(portfolio)
+
+            session.commit()
